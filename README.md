2215278f01b530cdb71378edcf3aa7111779b958
bryant-nguyen <bryantdnguyen@csu.fullerton.edu>
2023-03-21 23:38:46 -0700

    Changed README and added basic program.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cpsc254-lab6
+# Lab 6: Intro to Github
+Name: Bryant Nguyen
+CWID: 888614195
+
+This repo contains the file 'basic_arithmetic.cpp'. This file shows basic arithmetic operations in C++. 
---
+++ basic_arithmetic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+  
+int main() {
+    int result_add = 20 + 5;
+    int result_sub = 20 - 5;
+    int result_mult = 20 * 5;
+    int result_div =  20/5;
+
+    cout << "Result of 20 + 5: " << result_add << endl;
+    cout << "Result of 20 - 5: " << result_sub << endl;
+    cout << "Result of 20 * 5: " << result_mult << endl;
+    cout << "Result of 20 / 5: " << result_div << endl;
+    return 0;
+}
